7273e64dd31b69ac5da40dfbff304dc5d2c9d420
unknown <thiago.prando184@gmail.com>
2026-08-25 16:58:52 -0300

    Initial commit

---
+++ src/main/java/dao/VendedorDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import factory.ConnectionFactory;
+import model.Vendedor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
+    @Override
+    public void inserir(Vendedor entidade) {
+        String sql = "insert into java_vendedor(nome) values(?)";
+
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, entidade.getNome());
+            ps.execute(); // ps.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<Vendedor> listar() {
+        List<Vendedor> lista = new ArrayList<>();
+        String sql = "select * from java_vendedor";
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery()) {
+            while(rs.next()) {
+                Vendedor vendedor = new Vendedor();
+                vendedor.setId(rs.getInt("id"));
+                vendedor.setNome(rs.getString("nome"));
+                lista.add(vendedor);
+            }
+        }
+        catch(SQLException e) {
+
+        }
+
+        return lista;
+    }
+}
---
+++ src/main/java/view/MenuVendedor.java
@@ -0,0 +1,48 @@
+package view;
+
+import dao.VendedorDAO;
+import model.Vendedor;
+
+import java.util.List;
+
+import static javax.swing.JOptionPane.*;
+
+public class MenuVendedor {
+    public void menu() {
+        String[] item = {"Inserir", "Pesquisar", "Listar",
+                "Atualizar", "Excluir", "Sair"};
+        String opcao;
+
+        do {
+            opcao = (String) showInputDialog(null,
+                    "Selecione uma opção",
+                    "*** MENU VENDEDOR ***",
+                    INFORMATION_MESSAGE,
+                    null,
+                    item,
+                    item[0]);
+            switch(opcao.toLowerCase()) {
+                case "inserir" -> inserir();
+                case "listar" -> listar();
+            }
+        }
+        while(!opcao.toLowerCase().equals("sair"));
+
+    }
+
+    private void listar() {
+        List<Vendedor> lista = new VendedorDAO().listar();
+        String aux = "";
+        for(Vendedor vendedor : lista) {
+            aux += vendedor.getId() + "    | " + vendedor.getNome() + "\n";
+        }
+        showMessageDialog(null, aux);
+    }
+
+    private void inserir() {
+        Vendedor vendedor = new Vendedor();
+        String nome = showInputDialog("Nome");
+        vendedor.setNome(nome);
+        new VendedorDAO().inserir(vendedor);
+    }
+}
